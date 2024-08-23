4207a4fcc5c2f8c479a0527bb222d38c081a4eeb
Andres1Leon <oaleon06@soy.sena.edu.cogit config --global user.email oaleon06@soy.sena.edu.co>
2024-08-23 11:07:23 -0500

    Algoritmo Suma Numeros

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ if num % 2 == 0:
     print("El numero es par")
 else:
     print("El numero es impar")
+    
+cant = int(input("Cuantos datos va a ingresar: "))
+
+numeros = []
+
+for i in range(0, cant):
+    numeros.append(int(input("Digite un numero: ")))
+
+suma = sum(numeros)
+
+print(f"La suma total de los numeros es de: {suma}")
